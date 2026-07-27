41d0140eb9852002affb74a89a834f0bda7a2846
K-ingo <emilkkc@gmail.com>
2026-07-27 21:21:23 +0200

    inventar-v1-done-hotfix1

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,34 @@
 import Dexie from 'dexie';
 import type { Table } from 'dexie';
 
+export type ItemStatus = 'ejer' | 'overvejer' | 'solgt';
+
+export interface Garanti {
+  laengde_aar: number;
+  udloeber_dato: string;
+  paamindelse_dage: number;
+}
+
 export interface Item {
   id?: number;
   navn: string;
   vaegt_g: number;
   pris_kr: number;
+  dimensioner: string;
+  antal: number;
+  delt: boolean;
+  status: ItemStatus;
   tags: string[];
+  kraever: string[];
+  komplementer: string[];
+  koebt_hos: string;
+  koebsdato: string;
+  koebslink: string;
+  ordrenummer: string;
+  garanti: Garanti | null;
+  noter: string;
   oprettet: Date;
+  aendret: Date;
 }
 
 export class FeltbogenDB extends Dexie {
@@ -16,7 +37,7 @@ export class FeltbogenDB extends Dexie {
   constructor() {
     super('FeltbogenDB');
     this.version(1).stores({
-      items: '++id, navn, oprettet'
+      items: '++id, navn, status, oprettet'
     });
   }
 }
